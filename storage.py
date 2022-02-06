67e7bf4b5a03dc497a50eab2cd1ed0753d6f4dcb
HaIiax <jackgoehringer@gmail.com>
2022-02-06 17:34:16 -0500

    added major changes to storage.py

--- storage.py
+++ storage.py
@@ -1,3 +1,4 @@
+import boto3
 from classes import Person
 
 
@@ -5,7 +6,34 @@ class Storage:
     def __init__(self):
         self.persons = {}
         self.cars = {}
+        self.client = boto3.client('s3')
+
+        self.put('testing/Storage', 'a test value')
+        print(self.get('testing/Storage'))
+
+        print(self.get('testing/StorageNotFound'))
+
+        self.put('testing/StorageToBeDeleted', 'a test value to be deleted')
+        print(self.get('testing/StorageToBeDeleted'))
+        self.delete('testing/StorageToBeDeleted')
+
         print('Storage Initialized')
+
+    def get(self, key):
+        try:
+            sf = self.client.get_object(Bucket='battery-staple-v1', Key=key)
+            return sf["Body"].read().decode("utf-8")
+        except:
+            return None
+
+    def put(self, key, value):
+        self.client.put_object(Bucket='battery-staple-v1', Key=key, Body=value.encode('utf-8'))
+
+    def delete(self, key):
+        try:
+            self.client.delete_object(Bucket='battery-staple-v1', Key=key)
+        except:
+            return
 
     def stats(self):
         print ("len(self.persons): " + str(len(self.persons)))
