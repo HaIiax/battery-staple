d9c618dfff9194ab319bd55468a2540108692f7f
HaIiax <jackgoehringer@gmail.com>
2022-03-04 22:59:53 -0500

    fixed bug in security init

--- security.py
+++ security.py
@@ -1,9 +1,9 @@
 class Security:
-    def __init__(self, security_definition: str):
-        self.security_definition = security_definition.replace(" ", "")
+    def __init__(self, abnormal_security_definition: str):
+        self.security_definition = abnormal_security_definition.replace(" ", "")
         self.security_dict = {}
         if self.security_definition is not None:
-            for permitted_command_users in security_definition.split(";"):
+            for permitted_command_users in self.security_definition.split(";"):
                 permitted_users = permitted_command_users.split(",")
                 self.security_dict[permitted_users[0]] = permitted_users[1:]
 
